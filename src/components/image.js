b3f1149f9175ca2a26b183bf6c29f9c655d60afd
oliver archner <oliver.archner@uni-bayreuth.de>
2020-10-08 13:28:46 +0200

    1.4.1

--- src/components/image.js
+++ src/components/image.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export class Image extends React.Component {  
+export class Image extends React.Component {
+  
     render(){
-      var src = "data:/image/jpg;base64," + this.props.item_value
-      var width = this.props.prop.width ? this.props.prop.width : 100
-      var height = this.props.prop.height ? this.props.prop.height : 100
        return (
-           <img alt={this.props.prop.alt} src={src} width={width} height={height}></img>
+           <img alt={this.props.alt} src={this.props.item_value} width={this.props.width} height={this.props.height}></img>
        );
      }
 }
